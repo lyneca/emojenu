15256d4985f5581d5cfb2cd8aabb9d928e6a237f
Luke Tuthill <lukemtuthill@gmail.com>
2018-09-13 16:18:14 +1000

    First [late] commit

---
+++ generate.py
@@ -0,0 +1,30 @@
+import os
+
+def get_symbols(line):
+    codepoints = line.split(';')[0].strip().split()
+    keywords = ' '.join(line.split('#')[1].strip().split(' ')[1:])
+    return (codepoints, keywords)
+
+
+def get_emoji(codepoint):
+    return chr(int("0x" + codepoint, 16))
+
+
+def format_line(codepoints, desc):
+    return "{}|{}".format(desc, ''.join([get_emoji(x) for x in codepoints]))
+
+
+with open("emoji-test.txt") as f:
+    emoji_file = f.read()
+
+emoji = [x for x in emoji_file.split('\n') if not x.startswith("#") and x]
+
+emoji_list = [get_symbols(x) for x in emoji]
+
+path = os.path.join(os.environ["HOME"], '.config/emoji')
+if not os.path.exists(path):
+    open(path, 'x').close()
+
+with open('/home/lyneca/.config/emoji', 'w') as f:
+    for emoji in emoji_list:
+        f.write(format_line(*emoji) + "\n")
